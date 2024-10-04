a30cdc2e2deaa9e6fd08eeabb25e8cc214ce9677
kasinath <kasninathanilkumar@gmail.com>
2024-10-05 02:11:43 +0530

    navbar updated

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import ZymoLogo from '../CustomNavbar/ZymoLogo.png';
-import { Link } from 'react-router-dom';
+import { Link,NavLink } from 'react-router-dom';
 
 const CustomNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" sticky='top' style={{boxSizing:'border-box'}} className="shadow-sm px-24 bg-light">
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex align-items-center" style={{ width: '12rem' }}>
           <img src={ZymoLogo} className="img-fluid w-100" alt="Zymo Logo" />
@@ -13,10 +13,10 @@ const CustomNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link className="me-3" style={{ fontSize: '1.2rem', fontWeight: '400' }} href="#home">Deals</Nav.Link>
-            <Nav.Link className="me-3" style={{ fontSize: '1.2rem', fontWeight: '400' }} href="#fleets">Fleets</Nav.Link>
-            <Nav.Link className="me-3" style={{ fontSize: '1.2rem', fontWeight: '400' }} href="#about">About Us</Nav.Link>
-            <Nav.Link className="me-3" style={{ fontSize: '1.2rem', fontWeight: '400' }} href="#contact">Contact Us</Nav.Link>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/">Fleets</NavLink>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/cars">Deals</NavLink>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/zymo">About Us</NavLink>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/efer">Contact Us</NavLink>
           </Nav>
           <Nav className="ms-auto d-flex align-items-center">
             <Nav.Link href="#faq" style={{ fontSize: '1.2rem', fontWeight: '400' }} className="me-3">FAQ's</Nav.Link>
