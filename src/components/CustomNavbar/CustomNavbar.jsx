6aa75ccf99b216a11e2ac9a0e24e02699c6ef8b3
kasinath <kasninathanilkumar@gmail.com>
2024-10-05 16:48:26 +0530

    navbar updated

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -13,10 +13,11 @@ const CustomNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/">Fleets</NavLink>
+            <NavLink className="me-5 ms-2" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/">Fleets</NavLink>
             <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/cars">Deals</NavLink>
             <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/zymo">About Us</NavLink>
-            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/efer">Contact Us</NavLink>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/refer">Contact Us</NavLink>
+            <NavLink className="me-5" style={{ fontSize: '1.2rem', fontWeight: '400' }} to="/career">Career</NavLink>
           </Nav>
           <Nav className="ms-auto d-flex align-items-center">
             <Nav.Link href="#faq" style={{ fontSize: '1.2rem', fontWeight: '400' }} className="me-3">FAQ's</Nav.Link>
