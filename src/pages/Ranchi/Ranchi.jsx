7cce4faf69e06d9ffc58fdc743b18395321f9e28
adityakhade321 <adityakhadeak47@gmail.com>
2024-10-12 15:14:20 +0530

    Rename Ranchi component to Delhi and update styling; comment out Carousel implementation

--- src/pages/Ranchi/Ranchi.jsx
+++ src/pages/Ranchi/Ranchi.jsx
@@ -6,8 +6,7 @@ import p2 from '../../assets/p2.png'
 import p3 from '../../assets/p3.png'
 import Example from "../../components/Example/Example"
 
-
-const Ranchi = () => {
+const Delhi = () => {
   const [index, setIndex] = useState(0);
 
   // Custom interval for the carousel that works even when the browser is inactive
@@ -24,10 +23,52 @@ const Ranchi = () => {
   };
 
   return (
-    <div className='m-10'>
+    <div className='md:m-10 mx-3 my-2 font-poppins'>
       <Example />
+      {/* <Carousel activeIndex={index} onSelect={handleSelect} controls={false} indicators={false}>
+        <Carousel.Item>
+          <a href="http://get.zymo.app/car">
+            <img
+              className="d-block w-100 rounded p-1"
+              src={banner1}
+              alt="First slide"
+              style={{ border: 'solid' }}
+            />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="http://get.zymo.app/car">
+            <img
+              className="d-block w-100 rounded p-1"
+              src={p1}
+              alt="slide"
+              style={{ border: 'solid' }}
+            />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="http://get.zymo.app/car">
+            <img
+              className="d-block w-100 rounded p-1"
+              src={p2}
+              alt="Second slide"
+              style={{ border: 'solid' }}
+            />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="http://get.zymo.app/car">
+            <img
+              className="d-block w-100 rounded p-1"
+              src={p3}
+              alt="Third slide"
+              style={{ border: 'solid' }}
+            />
+          </a>
+        </Carousel.Item>
+      </Carousel> */}
     </div>
   );
 };
 
-export default Ranchi;
+export default Delhi;
