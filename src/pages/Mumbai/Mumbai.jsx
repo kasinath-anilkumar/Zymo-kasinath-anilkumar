1e9afcd49e2178d56c51a9fbb56a29abb990f93b
adityakhade321 <adityakhadeak47@gmail.com>
2024-10-30 15:14:59 +0530

    "Updated Hyderabad and Mumbai pages to use new city-specific components and logos"

--- src/pages/Mumbai/Mumbai.jsx
+++ src/pages/Mumbai/Mumbai.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import banner1 from '../../assets/banner1.jpg'
-import p4 from '../../assets/p4.png'
-import p2 from '../../assets/p2.png'
-import p3 from '../../assets/p3.png'
-import Banner from "../../components/Banner/Banner"
-
+import mumbai from "../../assets/City-Logo/mumbai.png"
+import CompareAndChoice from "../../components/city components/CompareAndChoice/CompareAndChoice";
+import Benefits from "../../components/city components/Benefits/Benefits";
+import BlogSection from "../../components/city components/BlogSection/BlogSection";
+import Banner from "../../components/city components/Banner/Banner";
 
 
 const Mumbai = () => {
   return (
     <>
-     <Banner/>
+      <Banner bg={mumbai}/>
+      <CompareAndChoice />
+      <Benefits />
+      <BlogSection cityName={"mumbai"} />
     </>
   );
 };
