30736caca9ab60f8ceefd85dce91ffb8e5032065
adityakhade321 <adityakhadeak47@gmail.com>
2024-11-08 14:33:07 +0530

    fix: correct import naming for Thane city logo

--- src/pages/Thane/Thane.jsx
+++ src/pages/Thane/Thane.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Thane from "../../assets/City-Logo/thane.png"
+import thane from "../../assets/City-Logo/thane.png"
 import CompareAndChoice from "../../components/city components/CompareAndChoice/CompareAndChoice";
 import Benefits from "../../components/city components/Benefits/Benefits";
 import BlogSection from "../../components/city components/BlogSection/BlogSection";
